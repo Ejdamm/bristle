73a138bf2a9a51bd839c22ea985de9c6b5066d6b
Adam Georgsson <adamgeorgsson@gmail.com>
2017-03-14 13:22:55 +0100

    added nav menu

--- index.php
+++ index.php
@@ -1,15 +1,15 @@
 <?php 
-include("src/conf.php");
-include("src/header.php");
-?>
+include 'src/conf.php';
+include 'src/header.php';
+
+include 'src/db.php';
+$db = new DB_connect;
+$db->connect();
+echo $db->getLastId("signature");
+	
 
 
 
 
 
-
-
-
-
-<?php
-include("src/footer.php");
+include 'src/footer.php';
--- src/header.php
+++ src/header.php
@@ -6,6 +6,14 @@
 		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Oxygen"> 
 	</head>
 	<body>
-		<header id="topheader">
+		<header class="topheader">
+			<span class="logo">Bristle</span>
+
+			<nav class="navbar">
+				<ul class="navbar-list">
+					<li><a href="index.php">Dashboard</a></li>
+					<li><a href="events.php">Events</a></li>
+				</ul>
+			</nav>
 		</header>
 		<main>
